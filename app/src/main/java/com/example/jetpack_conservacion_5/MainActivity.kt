d9a263d4a1f14d2b9d477b1ad48444bda70e8132
PC Casa <robpalmol@alu.edu.gva.es>
2023-10-25 21:02:55 +0200

    Commit final

--- app/src/main/java/com/example/jetpack_conservacion_5/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_conservacion_5/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.jetpack_conservacion_5
 
-import androidx.compose.foundation.clickable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -32,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     estimatedHours = 0,
                     actualHours = 0,
                     dueDate = "",
-                    completionDate = "",)
+                    completionDate = "")
             }
             val tasks = remember { mutableStateListOf<Task>() }
             tasks.addAll(defaultTasks)
@@ -45,7 +54,7 @@ data class Task(
     val id: Int,
     val name: String,
     var isCompleted: Boolean,
-    val priority: Priority,
+    var priority: Priority,  // Cambiado para que la prioridad sea modificable
     val estimatedHours: Int,
     val actualHours: Int,
     val dueDate: String,
@@ -56,9 +65,8 @@ enum class Priority {
     LOW, MEDIUM, HIGH, URGENT
 }
 
-
 @Composable
-fun TaskItem(task: Task, onUpdateTask: (Task) -> Unit, priority: Priority) {
+fun TaskItem(task: Task, onUpdateTask: (Task) -> Unit) {
     var editing by remember { mutableStateOf(false) }
     var editedName by remember { mutableStateOf(task.name) }
 
@@ -67,7 +75,7 @@ fun TaskItem(task: Task, onUpdateTask: (Task) -> Unit, priority: Priority) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .background(getBackgroundColorForPriority(priority = priority))
+            .background(getBackgroundColorForPriority(priority = task.priority))
     ) {
         Checkbox(
             checked = task.isCompleted,
@@ -101,14 +109,75 @@ fun TaskItem(task: Task, onUpdateTask: (Task) -> Unit, priority: Priority) {
             )
         } else {
             Text(
-                text = editedName+"\n" +
-                        "Prioridad: ${task.priority}"+"\n"+
-                "Horas estimadas: ${task.estimatedHours}"+"\n",
-                modifier = Modifier.weight(1f).clickable { editing = false }
+                text = editedName + "\n" +
+                        "Prioridad: ${task.priority}" + "\n" +
+                        "Horas estimadas: ${task.estimatedHours}" + "\n",
+                modifier = Modifier.weight(1f).clickable { editing = true }
             )
+        }
+
+        IconButton(
+            onClick = {
+                ShowPriorityDialog(task = task, onUpdateTask = onUpdateTask)
+            }
+        ) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Cambiar Prioridad")
         }
     }
 }
+
+@Composable
+fun ShowPriorityDialog(task: Task, onUpdateTask: (Task) -> Unit) {
+    val context = LocalContext.current
+    var selectedPriority by remember { mutableStateOf(task.priority) }
+    var dialogOpen by remember { mutableStateOf(true) }
+
+    AlertDialog(
+        { dialogOpen = false }, {
+            Button(
+                onClick = {
+                    // Actualiza la prioridad de la tarea y llama a onUpdateTask para reflejar los cambios
+                    task.priority = selectedPriority
+                    onUpdateTask(task)
+                    dialogOpen = false
+                }
+            ) {
+                Text("Guardar")
+            }
+        },
+        title = { Text("Seleccionar Prioridad") },
+        dismissButton = {
+            Button(
+                onClick = {
+                    dialogOpen = false
+                }
+            ) {
+                Text("Cancelar")
+            }
+        },
+        content = {
+            Priority.values().forEach { priority ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            selectedPriority = priority
+                        },
+                    content = {
+                        RadioButton(
+                            selected = (priority == selectedPriority),
+                            onClick = { selectedPriority = priority }
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(priority.name)
+                    }
+                )
+            }
+        }
+    )
+}
+
+
 fun getBackgroundColorForPriority(priority: Priority): Color {
     return when (priority) {
         Priority.LOW -> Color.Green
@@ -119,10 +188,10 @@ fun getBackgroundColorForPriority(priority: Priority): Color {
 }
 
 @Composable
-fun TaskList(tasks: List<Task>, onUpdateTask: (Task) -> Unit, priority: Priority) {
+fun TaskList(tasks: List<Task>, onUpdateTask: (Task) -> Unit) {
     Column {
         for (task in tasks) {
-            TaskItem(task = task, onUpdateTask = onUpdateTask, priority = priority)
+            TaskItem(task = task, onUpdateTask = onUpdateTask)
         }
     }
 }
@@ -151,16 +220,12 @@ fun MyApp(tasks: MutableList<Task>) {
         }
 
         item {
-            TaskList(
-                tasks = tasks,
-                onUpdateTask = { updatedTask ->
-                    val index = tasks.indexOfFirst { it.id == updatedTask.id }
-                    if (index != -1) {
-                        tasks[index] = updatedTask
-                    }
-                },
-                priority = Priority.LOW
-            )
+            TaskList(tasks = tasks) { updatedTask ->
+                val index = tasks.indexOfFirst { it.id == updatedTask.id }
+                if (index != -1) {
+                    tasks[index] = updatedTask
+                }
+            }
         }
 
         item {
@@ -197,18 +262,23 @@ fun MyApp(tasks: MutableList<Task>) {
         }
     }
 }
+
 @Preview
 @Composable
 fun MyAppPreview() {
     val tasks = List(20) { index ->
-        Task(id = index,
+        Task(
+            id = index,
             name = "Tarea ${index + 1}",
             isCompleted = false,
             priority = Priority.LOW,
             estimatedHours = 0,
             actualHours = 0,
             dueDate = "",
-            completionDate = "")
+            completionDate = ""
+        )
     }
     MyApp(tasks = tasks.toMutableList())
 }
+
+
